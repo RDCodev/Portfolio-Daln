f4b8a744f2081f08e056d750bc2a3f2d4fe24906
IIRDCodeII <rdcode.dev@gmail.com>
2025-02-06 21:03:00 -0500

    fix: Solve deploy issue on Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: cloudflare()
+  integrations: [tailwind(), react()]
 });
