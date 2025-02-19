10d18f27115cbc635c5a2233201ab6a48b4c9ade
IIRDCodeII <rdcode.dev@gmail.com>
2025-02-18 21:05:11 -0500

    fix: Update astro config and change imageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: "cloudflare"
+  })
 });
