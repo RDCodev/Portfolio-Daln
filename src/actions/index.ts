c45fce8afd8c7fb87287940576930bb34d11d821
Stalin D. Pillajo <StalinP@andelytica.com>
2025-02-25 10:15:01 -0500

    fix: Add Mustache template engine for email rendering and update dependencies

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,10 +4,10 @@ import { parseName } from "@utils/common";
 import { getSecret } from "astro:env/server"
 import Handlebars from "handlebars";
 import msgTemplate from "@resources/handlebars-templates/message-notification.hbs?raw";
+import Mustache from "mustache";
 import type { MailSenderValues } from "@ui/home/mail-sender-modal/mail-sender";
 
 const resend = new Resend(getSecret("RESEND_API_KEY"));
-const template = Handlebars.compile(msgTemplate);
 
 export const server = {
   send: defineAction({
@@ -18,7 +18,7 @@ export const server = {
         to: ["rkostalin@gmail.com"],
         from: "Daln.dev <server@daln.dev>",
         subject: "Hi there! You've got a new message!",
-        html: template({
+        html: Mustache.render(msgTemplate, {
           receipt: parseName(rest),
           receiptEmail: email,
           message
